ebc18632195ad3f0a15165aa8f50e3ae1f5d8c01
Tsubasa Nakayama <app.nakayama@gmail.com>
2018-06-30 20:54:32 +0900

    add tab bar controller

---
+++ SwiftP/NumberViewController.swift
@@ -0,0 +1,41 @@
+//
+//  NumberViewController.swift
+//  SwiftP
+//
+//  Created by 中山翼 on 2018/06/30.
+//  Copyright © 2018年 Tsubasa Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class NumberViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        // ステータスバーの文字色を白で指定
+        return UIStatusBarStyle.lightContent
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
